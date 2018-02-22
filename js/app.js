2d10bbfd912159fe0accd3a7fd19e4b3bc90db2b
pablo <pablo@300000kms.net>
2018-02-22 17:36:12 +0100

    tablas retocadas

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
 
 //var urljson ='http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_search';
-var urljson ='http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_search?rows=50'
-var urlcsv = 'http://opendata-ajuntament.barcelona.cat/data/cataleg.csv?public=true'
-var csv ='data/data.csv'
+//var urljson ='http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_search?rows=150'
+//var urlcsv = 'http://opendata-ajuntament.barcelona.cat/data/cataleg.csv?public=true'
+//var csv ='data/data.csv'
 
 
-function getData(){
-
+function getData(urljson){
     //http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_search?callback=jQuery112006745389475568797_1510911664869&_=1510911664870
     //http://opendata-ajuntament.barcelona.cat/data/api/3/action/package_show?id=trams&callback=lamevafuncio
-
+    console.log(urljson)
     $.ajaxSetup({'cache':true});
-
     $.ajax({
         url: urljson,
         dataType: "jsonp",
@@ -29,13 +27,8 @@ function parseData(data){
 
 function format(c, v){return v}
 
-function doTable(data){
-    console.log(data)
-    
+function doTable(data){    
     da = data.result.results
-    cols = Object.keys(data.result.results[0])
-    
-    cols = ['title', 'author', 'department', 'notes', ]
     
     html = '';
     $('body').append('<div class="tab"></div>');
@@ -44,57 +37,86 @@ function doTable(data){
     
     html += '<thead><tr>'
     html += '<th>id</th>'
-    for(c in cols){ html += '<th>'+cols[c]+'</th>' }
-    html += '<th>area</th>'
+    html += '<th>title</th>'
+    html += '<th>author</th>'
+    html += '<th>depatment</th>'
+    html += '<th>notes</th>'
+    html += '<th>description</th>'
+    html += '<th>downloads</th>'
     html += '<th>year</th>'
+    html += '<th>publication</th>'
     html += '<th>link</th>'
     html += '<th>view</th>'
-    html += '<th>get it!</th>'
+
     html += '</tr></thead>'
     
-    /*
-    html += '<tfoot><tr>'
-    html += '<th>id</th>'
-    for(c in cols){ html += '<th>'+cols[c]+'</th>' }
-    html += '<th>area</th>'
-    html += '<th>link</th>'
-    html += '<th>view</th>'
-    html += '<th>get it!</th>'
-    html += '</tr></tfoot>'
-    */
-    regex= /\d{4}/
-    html+='<tbody>'
+    dic = []
     for(d in da){
         for (r in da[d].resources){
-            year = regex.exec(da[d].resources[r].name);
-            if (year != null){year= year[0]}else{year=''}
+            year = da[d].resources[r].name.split('.')[0];
+            publication = da[d].resources[r].created.split("T")[0]
             downs = da[d].resources[r].downloads_absolute;
             if (downs=='None'){downs=''}
-            html += '<tr>'
-            html += '<td>'+d+'</td>'
-            for (c in cols){html += '<td><div class="cell">'+format(c, da[d][cols[c]])+'</div></td>'}
-            html += '<td><div class="cell">'+da[d].organization.description_translated.en+'</div></td>'
-            //html += '<td>'+da[d].resources[r].name+'</td>'
-            html += '<td>'+year+'</td>'
-            html += '<td><div class="format '+da[d].resources[r].format.toLowerCase()+'">'+da[d].resources[r].format.toLowerCase()+'</div>'
-            html += '<a href="'+da[d].resources[r].url+'"><i class="fa fa-arrow-down" aria-hidden="true"></i></a>'
-
-            html += '</td>' //link
-            if( ('CSV').indexOf(da[d].resources[r].format) != -1){
-                html += '<td><a href="'+da[d].resources[r].url+'">RAW</a></td>' //view
-            }else{
-                html += '<td></td>'
-            }
-            html += '<td>'+downs+'</td>'
-            html += '</tr>'
-
+            
+            dict={}
+            dict['i']=d
+            dict['title']=da[d]['title']
+            dict['author']=da[d]['author']
+            dict['department']=da[d]['department']
+            dict['notes']=da[d]['notes']
+            dict['descr']=da[d].organization.description_translated.en
+            dict['year']=year
+            dict['publication']=publication
+            dict['url']=da[d].resources[r].url
+            dict['downs']=downs
+            dic.push(dict)
         }
     }
     
+    var res = alasql('SELECT i, title, author, department, notes, descr, year, publication, SUM(url) as url, SUM(CAST(downs AS NUMBER)) as downs FROM ? GROUP BY i, title, author, department, notes, descr, year, publication',[dic]);       
+    
+    html+='<tbody>'
+    for(r in res){
+        html += '<tr>'
+        html += '<td>'+res[r].i+'</td>'
+        html += '<td><div class="cell">'+res[r].title+'</div></td>'
+        html += '<td><div class="cell">'+res[r].author+'</div></td>'
+        html += '<td><div class="cell">'+res[r].department+'</div></td>'
+        html += '<td><div class="cell">'+res[r].notes+'</div></td>'
+        html += '<td><div class="cell">'+res[r].descr+'</div></td>'
+        html += '<td>'+res[r].downs+'</td>'
+        html += '<td>'+res[r].year+'</td>'
+        html += '<td>'+res[r].publication+'</td>'
+        
+        if (res[r].url != undefined){
+            urls =res[r].url.split('http')
+            html += '<td>'
+            for (u in urls){
+                console.log(urls[u])
+                if ( ['', undefined].indexOf(urls[u])==-1){
+                    format = urls[u].split('.')[urls[u].split('.').length-1].toLowerCase()
+                    html += '<a href="'+'http'+urls[u]+'" class="format '+format+'">'+format+'</a>'
+                }
+            }
+            html += '</td>'
+        }else{html+='<td></td>'}
+        
+        if (res[r].url != undefined){
+            urls =res[r].url.split('http')
+            html += '<td>'
+            for (u in urls){
+                if ( ['', undefined].indexOf(urls[u])==-1){
+                    if(urls[u].split('.')[urls[u].split('.').length-1].toLowerCase()== 'csv'){html += '<i class="far fa-chart-bar"></i>'}
+                }
+            }
+            html += '</td>'
+        }else{html+='<td></td>'}
+        
+        html += '</tr>' 
+    }
     html += '</tbody>'
     html += '</table>'
     
-    //console.log(html)
     
     $('.tab').append(html)
     $('#tab').DataTable({
@@ -103,27 +125,25 @@ function doTable(data){
         bLengthChange: false,
         bInfo: false,
         bAutoWidth: true,
-        scrollY: $("body").height()-150,
-        //pageLength: 10,
-        //paging: false,
+        //scrollY: $("body").height()-150,
+        pageLength: 50,
+        paging: true,
         "deferRender": true,
         "searching": true,
-        rowGroup: {
-            dataSrc: 1
-        },
+        rowGroup: { dataSrc: 1 },
         responsive: {
             details: true
         },
         //columns: [null, null, null, null, null, null, {width: "100%" }]
     });
     
-    //popups
+    console.log($('#tab_filter label').text())
+    $('#tab_filter label input').attr('placeholder', 'Search')
     popups()
 }
 
 function popups(){
     $('body').append('<div id="popup"></div>');
-    
     $('.cell').hover(function(event){
         //console.log(event)
         $('#popup').html($(this).text());  
@@ -133,10 +153,20 @@ function popups(){
     }, function(){
         //$('#popup').fadeOut();
     })
-    
-
 }
 
-///https://datatables.net/examples/plug-ins/range_filtering.html
-//https://stackoverflow.com/questions/26973570/setting-a-max-character-length-in-css
-getData()
+function conf(data){
+    $('body').append('<div id="main"><div id="city"></div></div>');
+    $('#city').html(data.name)
+}
+
+$(document).ready(function () {
+    city = location.hash;
+    if(city==''){city = 'barcelona'}
+    console.log(city)
+    $.getJSON( "cities/"+city+".json", function( data ) {
+        console.log(data)
+        getData(data.url)
+        conf(data)
+    })
+})
